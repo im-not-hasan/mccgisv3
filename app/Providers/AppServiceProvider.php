9ce94c72800be1a24047782bf43cf2c1c66eff94
Hasan <imnothasan.ph@gmail.com>
2025-09-11 16:02:08 +0800

    Revert 'Exclude Pail'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->environment('local')) {
-            $this->app->register(\Laravel\Pail\PailServiceProvider::class);
-        }
+        //
     }
 
     /**
