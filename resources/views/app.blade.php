c3ace9316bcdf88e957760cf3929981b32a00c1d
Hasan <imnothasan.ph@gmail.com>
2025-09-16 20:06:46 +0800

    Google ReCaptcha Token update

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,11 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="recaptcha-site-key" content="{{ config('services.recaptcha.site_key') }}">
+        <meta name="recaptcha-site-key" content="{{ env('RECAPTCHA_SITE_KEY') }}">
+
+
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
         
-        <script src="https://www.google.com/recaptcha/api.js?render={{ config('services.recaptcha.site_key') }}"></script>
+        <script src="https://www.google.com/recaptcha/api.js?render={{ env('RECAPTCHA_SITE_KEY') }}"></script>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
