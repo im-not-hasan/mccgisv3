453b8ce4c22d61e64f9271956ede9d49107de9f1
im-not-hasan <imnothasan.ph@gmail.com>
2025-09-19 13:31:57 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://www.google.com/recaptcha/api.js?render={{ env('VITE_RECAPTCHA_SITE_KEY') }}"></script>
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
