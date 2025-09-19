3bc54c729d36ead49a968af600474dd3b85612cf
im-not-hasan <imnothasan.ph@gmail.com>
2025-09-19 13:29:31 +0800

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="recaptcha-site-key" content="{{ env('RECAPTCHA_SITE_KEY') }}">
+        <meta name="recaptcha-site-key" content="{{ env('VITE_RECAPTCHA_SITE_KEY') }}">
 
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
